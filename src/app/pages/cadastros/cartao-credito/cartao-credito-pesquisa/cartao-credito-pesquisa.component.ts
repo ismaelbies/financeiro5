afbc8b2dd9ba2b525434dbf44d047e9a23b22813
= <=>
2022-08-03 16:48:03 -0300

    cartao credito pesquisa typeScript

--- src/app/pages/cadastros/cartao-credito/cartao-credito-pesquisa/cartao-credito-pesquisa.component.ts
+++ src/app/pages/cadastros/cartao-credito/cartao-credito-pesquisa/cartao-credito-pesquisa.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogService } from 'primeng/dynamicdialog';
+import { CartaoCredito } from 'src/app/models/cartao-credito';
+import { CartaoCreditoService } from 'src/app/service/cartao-credito.service';
+import { CartaoCreditoCadastroComponent } from '../cartao-credito-cadastro/cartao-credito-cadastro.component';
 
 @Component({
   selector: 'app-cartao-credito-pesquisa',
@@ -7,9 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartaoCreditoPesquisaComponent implements OnInit {
 
-  constructor() { }
+  public cartaoCreditoArray: CartaoCredito[] = [];
+  public array = [];
+  public total: number = 0;
+  public displayDeleteModal: boolean = false;
+  public displayDetailsModal: boolean = false;
+  public title = 'Cartão Créditos';
+  public detailsModalTitle = 'Visualizar Cartão Créditos';
+  public cartaoCreditoDelete = {} as any;
+  public cartaoCredito = {} as CartaoCredito;
+
+  constructor(
+    private cartaoCreditoService: CartaoCreditoService,
+    private dialog: DialogService
+    ) { }
 
   ngOnInit(): void {
+    this.findCartaoCredito();
   }
 
+  public findCartaoCredito() {
+    this.cartaoCreditoService.findAll().subscribe((cartaoCreditoArray: CartaoCredito[]) => {
+      this.cartaoCreditoArray = cartaoCreditoArray;
+      this.total = cartaoCreditoArray.length;
+    });
+  }
+
+  async openDialog(id: number): Promise<void> {
+    let headerTitle = 'Inserir Conta Corrente';
+    if(id > 0) headerTitle = 'Editar Conta Corrente';
+    const data = {
+      id: (id),
+    };
+    const dialogRef = this.dialog.open(CartaoCreditoCadastroComponent, {
+      width: '60%',
+      dismissableMask: false,
+      data,
+      header: headerTitle
+    }).onClose.subscribe(() => { this.findCartaoCredito() });
+  }
+
+  deleteRow(array: CartaoCredito) {
+    this.cartaoCreditoDelete = array;
+    this.displayDeleteModal = true;
+  }
+
+  closeDeleteModal() {
+    this.displayDeleteModal = false;
+  }
+
+  closeDetailsModal() {
+    this.displayDetailsModal = false;
+  }
+
+  deleteCartaoCredito() {
+    this.cartaoCreditoService.delete(this.cartaoCreditoDelete).subscribe((resultado) => {
+      this.findCartaoCredito();
+      this.displayDeleteModal = false;
+    })
+  }
+
+  mostrarDetalhes(array: any) {
+    this.displayDetailsModal = true;
+    this.cartaoCredito = array;
+    // this.banco = this.conta.banco;
+  }
 }
